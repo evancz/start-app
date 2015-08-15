5ff45bae83aa670e4ddce099bb1a68f3bf7a01b7
Evan Czaplicki <info@elm-lang.org>
2015-08-15 00:15:06 -0700

    Fix bad import in documentation

--- src/StartApp/Simple.elm
+++ src/StartApp/Simple.elm
@@ -52,7 +52,7 @@ programs like this [here](https://github.com/evancz/elm-architecture-tutorial/).
 
     import Html exposing (div, button, text)
     import Html.Events exposing (onClick)
-    import StartApp
+    import StartApp.Simple as StartApp
 
     main =
       StartApp.start { model = model, view = view, update = update }
